c7a74f740c687f0911cbcdf9744622fb84aaa2a6
David English <david@operaonfilm.com>
2014-10-15 23:47:25 -0700

    Added proper accessors and switch most of the variable names to snake case

--- app.rb
+++ app.rb
@@ -1,20 +1,24 @@
 class CircleCoordinatesGen
 
-  def initialize(numberOfCoordinatesToGenerate, limit)
-    @numberOfCoordinatesToGenerate = numberOfCoordinatesToGenerate
+  attr_accessor :number_of_coordinates_to_generate
+  attr_accessor :limit
+
+  Two = 2
+
+  def initialize(number_of_coordinates_to_generate, limit)
+    @number_of_coordinates_to_generate = number_of_coordinates_to_generate
     @limit = limit
-    @TWO = 2
   end
 
-  def generateCoordinates()
+  def generate_coordinates()
     arr = []
-    (0..@numberOfCoordinatesToGenerate).each do
-      radiusCondition = false
-      while !radiusCondition do
-        x = generateCoordinate()
-        y = generateCoordinate()
-        if calculateRadius(x,y) <= @limit
-          radiusCondition = true
+    (0..@number_of_coordinates_to_generate).each do
+      radius_condition = false
+      while !radius_condition do
+        x = generate_coordinate()
+        y = generate_coordinate()
+        if calculate_radius(x,y) <= @limit
+          radius_condition = true
         end
       end
       arr.push([x,y])
@@ -22,12 +26,12 @@ class CircleCoordinatesGen
     arr
   end
 
-  def generateCoordinate()
+  def generate_coordinate()
     rand((-@limit)..@limit)
   end
 
-  def calculateRadius(x,y)
-    Math.sqrt(x.abs ** @TWO + y.abs ** @TWO)
+  def calculate_radius(x,y)
+    Math.sqrt(x.abs ** Two + y.abs ** Two)
   end
 
 end
