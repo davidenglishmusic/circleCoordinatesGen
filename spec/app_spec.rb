1c5b9279302f6d789ad5ba8c725e17fd4a2032b1
David English <david@operaonfilm.com>
2015-01-13 08:31:40 -0800

    Added two specs

---
+++ spec/app_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../app'
+
+describe CircleCoordinatesGen do
+  before :all do
+    @test_object_001 = CircleCoordinatesGen.new(5, 15)
+  end
+
+  describe 'generate coordinates' do
+    it 'returns 5 coordinates when the object is set for 5 coordinates' do
+      expect(@test_object_001.generate_coordinates.size).to eq(5)
+    end
+  end
+
+  describe 'calculate_radius' do
+    it 'returns the correct radius based on an x ( width ) and y ( height ) coordinate' do
+      expect(@test_object_001.calculate_radius(6, 5)).to eq(7.810249675906654)
+    end
+  end
+end
